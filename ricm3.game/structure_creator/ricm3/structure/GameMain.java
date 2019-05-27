f7ee944bfdd504263fbf1ac700bc0d20e6ea6560
Fougere <sebastian71f@gmail.com>
2019-05-27 14:44:32 +0200

    GameMain dans game

--- ricm3.game/structure_creator/ricm3/structure/GameMain.java
+++ ricm3.game/structure_creator/ricm3/structure/GameMain.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package ricm3.game;
+package ricm3.structure;
 
 import java.awt.Dimension;
 
