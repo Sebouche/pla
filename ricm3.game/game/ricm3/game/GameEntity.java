67840f2661f5365d5e06e83f0dcdd972bba8b7f5
Fougere <sebastian71f@gmail.com>
2019-06-03 10:30:59 +0200

    Creation des fichiers principaux du jeu et journal de bord

---
+++ ricm3.game/game/ricm3/game/GameEntity.java
@@ -0,0 +1,14 @@
+package ricm3.game;
+
+import java.awt.image.BufferedImage;
+
+public class GameEntity {
+
+	int m_x,m_y,m_h,m_w;
+	BufferedImage[] m_sprites;
+	Model m_model;
+	//Automate m_automate;
+	public GameEntity() {
+		
+	}
+}
