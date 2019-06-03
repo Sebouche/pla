f2f474ad686051c0a4fd5f07c75188230ec6c1f8
Oshiina <nicolaslabbe2000@gmail.com>
2019-06-03 10:38:11 +0200

    Copie interpréteur

---
+++ ricm3.game/interpreter/ricm3/interpreter/IAction.java
@@ -0,0 +1,58 @@
+package ricm3.interpreter;
+
+/* Michael PÉRIN, Verimag / Univ. Grenoble Alpes, may 2019 */
+
+import ricm3.parser.Ast.*;
+
+public class IAction {
+	
+	IAction(){}
+	
+	boolean exec(Entity e){
+		return true;
+	}
+	
+	
+	
+	
+	public class Hit extends IAction {
+		Direction direction ;
+		Integer power;
+		
+		Hit(Direction direction, Integer power){
+			this.direction = direction ;
+			this.power = power;
+		}
+		Hit(Direction direction){
+			this.direction = direction ;
+			this.power = 1 ; // valeur par défaut
+		}
+		Hit(){
+		//	this.direction =  // Front par défaut
+			this.power = 1 ; // puissance par défaut
+		}
+		
+		boolean exec(Entity e){
+			e.hit(this.direction, max(e.power,this.power));
+		}
+	}
+	
+	
+	public class Move extends IAction {
+		Direction direction ;
+		
+		Move(){
+			// this.direction = ;// Front par défaut
+		}
+		
+		Move(Direction direction){
+			this.direction = direction ;
+		}
+
+		
+		boolean exec(Entity e){
+			e.move(this.direction) ;
+		}
+	}
+	
+}
---
+++ ricm3.game/interpreter/ricm3/interpreter/ICondition.java
@@ -0,0 +1,47 @@
+package ricm3.interpreter;
+
+import ricm3.parser.Ast.Condition;
+import ricm3.parser.Ast.*;
+
+/* Michael PÉRIN, Verimag / Univ. Grenoble Alpes, may 2019 */
+
+public class ICondition {
+
+	ICondition(){}
+	boolean eval(Entity e) { return true; } // à redéfinir dans chaque sous-classe
+
+	public class True extends ICondition {
+		True(){}
+		boolean eval(Entity e) { return true; }
+	}
+
+	public class Cell extends ICondition {
+		Direction direction ;
+		Kind kind ;
+		Distance distance ;
+		
+		Cell(Direction direction, Kind kind, Distance distance){
+			this.direction = direction ;
+			this.kind = kind ;
+			this.distance = distance ;
+		}
+		
+		Cell(Direction direction, Kind kind){
+			this.direction = direction ;
+			this.kind = kind ;
+			this.distance = 1 ;
+		}
+		
+		boolean eval(Entity e) { 
+			return is_Kind(this.kind, this.direction, this.distance, e.position, e.map) ;
+		}
+	}
+	
+	public class GotPower extends Condition {
+		GotPower(){}
+		boolean eval(Entity e) {
+			return (e.power > 0) ;
+		}
+	}
+
+}
