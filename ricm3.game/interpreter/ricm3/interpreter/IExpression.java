ff28ce3ecd3671a9bf07c325b67f2cbcc0382049
Oshiina <nicolaslabbe2000@gmail.com>
2019-06-06 16:43:08 +0200

    désolé sacha

--- ricm3.game/interpreter/ricm3/interpreter/IExpression.java
+++ ricm3.game/interpreter/ricm3/interpreter/IExpression.java
@@ -1,9 +1,15 @@
 package ricm3.interpreter;
 
+import ricm3.game.GameEntity;
+
 public class IExpression {
 	
 	public IExpression() {
 		
+	}
+
+	public boolean eval(GameEntity e) {
+		return true;
 	}
 
 }
