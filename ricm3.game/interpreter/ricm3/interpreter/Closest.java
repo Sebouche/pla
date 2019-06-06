dea08ba7689d480eef18d17bc310d2a156db1be7
Oshiina <nicolaslabbe2000@gmail.com>
2019-06-06 15:50:38 +0200

    Toutes les conditions finies, Tests à effectuer

--- ricm3.game/interpreter/ricm3/interpreter/Closest.java
+++ ricm3.game/interpreter/ricm3/interpreter/Closest.java
@@ -12,20 +12,37 @@ public class Closest extends ICondition {
 		this.type = Type.strToType(e);
 		this.dir = Direction.strToDir(dir);
 	}
+	
+	Direction dirClosest(GameEntity e, GameEntity f) {
+		int x = e.x()-f.x();
+		int y = e.y()-f.y();
+		if(Math.abs(x)>Math.abs(y)) {
+			if (x > 0) return Direction.WEST;
+			return Direction.EAST;
+		}
+		if (y>0) return Direction.SOUTH;
+		return Direction.NORTH;
+	}
 
 	@Override
 	public boolean eval(GameEntity e) {
+		double min = -1;
+		GameEntity close = null;
 		Iterator<GameEntity> iter = e.entities().iterator();
 		while (iter.hasNext()) {
 			GameEntity f = iter.next();
-			if(f.type() == type) {
-				
+			if (f.type() == type) {
+				if (e.distance(f) < min || min < 0) {
+					min = e.distance(f);
+					close = f;
+				}
 			}
 		}
+		if (close == null) return false;
 		
-		return false;
+		
+		
+
+		return dir == dirClosest(e,close);
 	}
 }
-
-
-// vraie si la plus proche entité est dans la direction
