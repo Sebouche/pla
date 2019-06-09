3b97ab7f5da55a562409890a9d40c1902579ba85
Fougere <sebastian71f@gmail.com>
2019-06-09 18:01:40 +0200

    Affichage du sous-terrain corrigé (sur les  cotés)

--- ricm3.game/game/ricm3/game/UndergroundWorld.java
+++ ricm3.game/game/ricm3/game/UndergroundWorld.java
@@ -16,7 +16,7 @@ public class UndergroundWorld extends World {
 
 	public UndergroundWorld(Model model) {
 		super(model);
-		m_grid = new GameEntity[20][60];
+		m_grid = new GameEntity[60][60];
 		m_player = new Player(m_model, 10, 10, 9999, m_model.m_sprites.get("scientist"), null, this);
 		generate_level();
 		m_bgmfile = new File("sprites/bgm_underground.wav");
@@ -24,24 +24,26 @@ public class UndergroundWorld extends World {
 
 	void generate_level() {
 		int randint;
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < 60; i++) {
 			for (int j = 0; j < 60; j++) {
 				m_grid[i][j] = null;
 			}
 		}
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < 60; i++) {
 			for (int j = 0; j < 60; j++) {
 				if (m_grid[i][j] == null) {
 					randint = m_rand.nextInt(1001);
 					if (randint >= 0 && randint < 900) {
 						Block b = new Block(m_model, (int) (j * Options.Entity_size * Options.Scale),
-								(int) (i * Options.Entity_size * Options.Scale), 300, m_model.m_sprites.get("block"),new IAutomaton(m_model.m_automatons.get(0)), m_model.m_undergroundworld);
+								(int) (i * Options.Entity_size * Options.Scale), 300, m_model.m_sprites.get("block"),
+								new IAutomaton(m_model.m_automatons.get(0)), m_model.m_undergroundworld);
 						b.set_idsprite(0);
 						m_grid[i][j] = b;
 					}
 					if (randint >= 900 && randint < 950) {
 						Block b = new Block(m_model, (int) (j * Options.Entity_size * Options.Scale),
-								(int) (i * Options.Entity_size * Options.Scale), 300, m_model.m_sprites.get("block"),new IAutomaton(m_model.m_automatons.get(0)),m_model.m_undergroundworld);
+								(int) (i * Options.Entity_size * Options.Scale), 300, m_model.m_sprites.get("block"),
+								new IAutomaton(m_model.m_automatons.get(0)), m_model.m_undergroundworld);
 						b.set_idsprite(5);
 						m_grid[i][j] = b;
 						for (int k = -1; k <= 1; k++) {
@@ -49,7 +51,8 @@ public class UndergroundWorld extends World {
 							if (randint2 <= 3 && i + k >= 0 && i + k < 20) {
 								b = new Block(m_model, (int) (j * Options.Entity_size * Options.Scale),
 										(int) ((i + k) * Options.Entity_size * Options.Scale), 300,
-										m_model.m_sprites.get("block"),new IAutomaton(m_model.m_automatons.get(0)),m_model.m_undergroundworld);
+										m_model.m_sprites.get("block"), new IAutomaton(m_model.m_automatons.get(0)),
+										m_model.m_undergroundworld);
 								b.set_idsprite(5);
 								m_grid[i][j] = b;
 							}
@@ -57,7 +60,8 @@ public class UndergroundWorld extends World {
 							if (randint2 <= 3 && j + k >= 0 && j + k < 60) {
 								b = new Block(m_model, (int) ((j + k) * Options.Entity_size * Options.Scale),
 										(int) (i * Options.Entity_size * Options.Scale), 300,
-										m_model.m_sprites.get("block"),new IAutomaton(m_model.m_automatons.get(0)),m_model.m_undergroundworld);
+										m_model.m_sprites.get("block"), new IAutomaton(m_model.m_automatons.get(0)),
+										m_model.m_undergroundworld);
 								b.set_idsprite(5);
 								m_grid[i][j] = b;
 							}
@@ -68,7 +72,8 @@ public class UndergroundWorld extends World {
 					}
 					if (randint >= 999) {
 						m_grid[i][j] = new Block(m_model, (int) (j * Options.Entity_size * Options.Scale),
-								(int) (i * Options.Entity_size * Options.Scale), 300, m_model.m_sprites.get("grassbg"),new IAutomaton(m_model.m_automatons.get(0)),m_model.m_undergroundworld);
+								(int) (i * Options.Entity_size * Options.Scale), 300, m_model.m_sprites.get("grassbg"),
+								new IAutomaton(m_model.m_automatons.get(0)), m_model.m_undergroundworld);
 					}
 				}
 			}
@@ -78,7 +83,7 @@ public class UndergroundWorld extends World {
 	@Override
 	public void step() {
 		gravity(m_player);
-		
+
 		int entity_size = (int) (Options.Scale * Options.Entity_size);
 		int distance = 10;
 		int pos_x = (int) (m_model.m_player.m_x / (Options.Entity_size * Options.Scale));
@@ -93,31 +98,31 @@ public class UndergroundWorld extends World {
 			}
 		}
 		/*
-		//collision avec les blocs autour du joueur
-		if (m_grid[pos_y - 1][pos_x] != null)
-			m_player.collision(m_grid[pos_y - 1][pos_x], m_player.m_dx, m_player.m_dy);
-		if (m_grid[pos_y - 1][pos_x_r] != null)
-			m_player.collision(m_grid[pos_y - 1][pos_x_r], m_player.m_dx, m_player.m_dy);
-		if (m_grid[pos_y][Math.floorMod((pos_x - 1), 60)] != null)
-			m_player.collision(m_grid[pos_y][Math.floorMod((pos_x - 1), 60)], m_player.m_dx, m_player.m_dy);
-		if (m_grid[pos_y][Math.floorMod((pos_x_r + 1), 60)] != null)
-			m_player.collision(m_grid[pos_y][Math.floorMod((pos_x_r + 1), 60)], m_player.m_dx, m_player.m_dy);
-		if (m_grid[pos_y + 1][Math.floorMod((pos_x - 1), 60)] != null)
-			m_player.collision(m_grid[pos_y + 1][Math.floorMod((pos_x - 1), 60)], m_player.m_dx, m_player.m_dy);
-		if (m_grid[pos_y + 1][Math.floorMod((pos_x_r + 1), 60)] != null)
-			m_player.collision(m_grid[pos_y + 1][Math.floorMod((pos_x_r + 1), 60)], m_player.m_dx, m_player.m_dy);
-		if (m_grid[pos_y_d + 1][pos_x] != null)
-			m_player.collision(m_grid[pos_y_d + 1][pos_x], m_player.m_dx, m_player.m_dy);
-		if (m_grid[pos_y_d + 1][pos_x_r] != null)
-			m_player.collision(m_grid[pos_y_d + 1][pos_x_r], m_player.m_dx, m_player.m_dy);
-		*/
+		 * //collision avec les blocs autour du joueur if (m_grid[pos_y - 1][pos_x] !=
+		 * null) m_player.collision(m_grid[pos_y - 1][pos_x], m_player.m_dx,
+		 * m_player.m_dy); if (m_grid[pos_y - 1][pos_x_r] != null)
+		 * m_player.collision(m_grid[pos_y - 1][pos_x_r], m_player.m_dx, m_player.m_dy);
+		 * if (m_grid[pos_y][Math.floorMod((pos_x - 1), 60)] != null)
+		 * m_player.collision(m_grid[pos_y][Math.floorMod((pos_x - 1), 60)],
+		 * m_player.m_dx, m_player.m_dy); if (m_grid[pos_y][Math.floorMod((pos_x_r + 1),
+		 * 60)] != null) m_player.collision(m_grid[pos_y][Math.floorMod((pos_x_r + 1),
+		 * 60)], m_player.m_dx, m_player.m_dy); if (m_grid[pos_y +
+		 * 1][Math.floorMod((pos_x - 1), 60)] != null) m_player.collision(m_grid[pos_y +
+		 * 1][Math.floorMod((pos_x - 1), 60)], m_player.m_dx, m_player.m_dy); if
+		 * (m_grid[pos_y + 1][Math.floorMod((pos_x_r + 1), 60)] != null)
+		 * m_player.collision(m_grid[pos_y + 1][Math.floorMod((pos_x_r + 1), 60)],
+		 * m_player.m_dx, m_player.m_dy); if (m_grid[pos_y_d + 1][pos_x] != null)
+		 * m_player.collision(m_grid[pos_y_d + 1][pos_x], m_player.m_dx, m_player.m_dy);
+		 * if (m_grid[pos_y_d + 1][pos_x_r] != null) m_player.collision(m_grid[pos_y_d +
+		 * 1][pos_x_r], m_player.m_dx, m_player.m_dy);
+		 */
 		Iterator<GameEntity> iter = m_entities.iterator();
 		GameEntity E;
 		while (iter.hasNext()) {
 			E = iter.next();
 			E.step();
 		}
-		
+
 	}
 
 	void gravity(MovingEntity ge) {
@@ -136,17 +141,17 @@ public class UndergroundWorld extends World {
 		int pos_y = (int) (m_model.m_player.m_y / (Options.Entity_size * Options.Scale));
 		for (int i = -distance; i <= distance; i++) {
 			for (int j = -distance; j <= distance; j++) {
-				k++;
 				if (m_grid[Math.floorMod((pos_y + i), 20)][Math.floorMod((pos_x + j), 60)] != null) {
-					Graphics g_child = g.create(k * (int) (Options.Entity_size * Options.Scale) - (cam_x % (int) (Options.Entity_size * Options.Scale)),
-							m_grid[Math.floorMod((pos_y + i), 20)][Math.floorMod((pos_x + j), 60)].m_y - (cam_y % (int) (Options.Entity_size * Options.Scale))
-									+ m_model.m_height / 2 + 2,
+					Graphics g_child = g.create(
+							(j * (int) (Options.Entity_size * Options.Scale))
+									- (cam_x % (int) (Options.Entity_size * Options.Scale)) + m_model.m_width / 2,
+							(i * (int) (Options.Entity_size * Options.Scale))
+									- (cam_y % (int) (Options.Entity_size * Options.Scale)) + m_model.m_height/2,
 							(int) (Options.Entity_size * Options.Scale), (int) (Options.Entity_size * Options.Scale));
 					m_grid[Math.floorMod((pos_y + i), 20)][Math.floorMod((pos_x + j), 60)].paint(g_child);
 					g_child.dispose();
 				}
 			}
-			k = 0;
 		}
 		Iterator<GameEntity> iter = m_entities.iterator();
 		GameEntity E;
