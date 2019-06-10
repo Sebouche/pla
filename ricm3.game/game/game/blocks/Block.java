64fe4e25e42f93683f71b66fa29442deff10782d
Fougere <sebastian71f@gmail.com>
2019-06-10 12:47:36 +0200

    Ajout d'un champ indestructible aux blocs

--- ricm3.game/game/game/blocks/Block.java
+++ ricm3.game/game/game/blocks/Block.java
@@ -7,9 +7,13 @@ import ricm3.game.*;
 import ricm3.interpreter.*;
 
 public class Block extends GameEntity {
+	boolean m_breakable=true;
 
 	public Block(Model model, int x, int y, int hp, BufferedImage[] sprites, IAutomaton automate, World originWorld) {
 		super(model, x, y, hp, sprites, automate, originWorld);
+		if(hp==-1) {
+			m_breakable=false;
+		}
 	}
 	
 	public void set_idsprite(int id) {
